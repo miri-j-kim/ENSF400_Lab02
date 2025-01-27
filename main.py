65f55d00894ced2f6730950a8da2b22423fe43f0
miri-j-kim <mirikim1411@gmail.com>
2025-01-27 20:50:26 +0000

    fixed some errors

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from github import Github
 
 # or using an access token
-g = Github("your_access_token")
+g = Github("token")
 repo = g.get_repo("miri-j-kim/ENSF400_Lab02")
 
 #Print all branches
@@ -9,16 +9,13 @@ print(list(repo.get_branches()))
 
 print(repo)
 
-#Get pull requests by number
-pr = repo.get_pull(664)
-pr
-
 #Get pull requests by query
-pulls = repo.get_pulls(state='open', sort='created', base='master')
+pulls = repo.get_pulls(state='closed', sort='created', base='main')
 for pr in pulls:
-   print(pr.number)
+   print(pr)
 
 #Task 3: List commits on main
 main_branch = repo.get_branch("main")
 commits = list(repo.get_commits(sha=main_branch.commit.sha))
-print(commits)
+for commit in commits:
+   print(commit)
