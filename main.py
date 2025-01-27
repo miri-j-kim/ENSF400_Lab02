7dbf95f1ce5fea62e4261a496432db0786943544
JoanOsike <144162334+JoanOsike@users.noreply.github.com>
2025-01-27 13:47:01 -0700

    Added comments

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ pulls = repo.get_pulls(state='open', sort='created', base='master')
 for pr in pulls:
    print(pr.number)
 
+#Task 3: List commits on main
 main_branch = repo.get_branch("main")
 commits = list(repo.get_commits(sha=main_branch.commit.sha))
 print(commits)
