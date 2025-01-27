204f9277f82c5fccffb3186e3e39721a8f6c5684
miri-j-kim <mirikim1411@gmail.com>
2025-01-27 21:43:04 +0000

    "Formatted output"

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 from github import Github
 
 # or using an access token
-g = Github("token")
+g = Github("token here")
 repo = g.get_repo("miri-j-kim/ENSF400_Lab02")
-
-#Print all branches
-print(list(repo.get_branches()))
 
 print(repo)
 
+#Print all branches
+print("\nTask 1 | Branches:")
+branches = repo.get_branches()
+for branch in branches:
+   print(branch)
+
 #Get pull requests by query
+print("\nTask 2 | Pull Requests:")
 pulls = repo.get_pulls(state='closed', sort='created', base='main')
 for pr in pulls:
    print(pr)
 
 #Task 3: List commits on main
+print("\nTask 3 | Commits:")
 main_branch = repo.get_branch("main")
 commits = list(repo.get_commits(sha=main_branch.commit.sha))
 for commit in commits:
-   print(commit)
+   print(commit, end=': ')
+   print(commit.author)
